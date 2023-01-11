9877f9fd767ad8f0dae08f38ee7dffea33539488
Kushaz Sehgal <kushaz.sehgal@gmail.com>
2023-01-11 15:21:26 +0530

    Done without brackets

--- assgn1/client.c
+++ assgn1/client.c
@@ -8,10 +8,13 @@
 #include<unistd.h>
 #include <stdbool.h>
 #define PORT_NUM 1301
+#define PACKET_SIZE 20
+
 
 int main(){
-    char buffer[200];
-    for(int i = 0;i < 200;i++)
+    char buffer[PACKET_SIZE];
+    char recv_buffer[PACKET_SIZE];
+    for(int i = 0;i < PACKET_SIZE;i++)
         buffer[i] = '\0';
     int sockfd;
     struct sockaddr_in serv_addr;
@@ -33,20 +36,40 @@ int main(){
     else{
         printf("Connection with sever established\n");
     }
-    bool continue_connection = true;
-    while(continue_connection){
-        int num;
-        printf("Enter a number : ");
-        scanf("%d",&num);
-        snprintf(buffer, 10, "%d", num);
-        // printf("%s",buffer);
+    while(1){
+    
+        printf("Enter an expression (enter -1 to exit) : ");
         
-        send(sockfd,buffer,strlen(buffer)+1,0);
-        if(num == -1){
-            continue_connection = false;
+        int i = 0;
+        char c;
+        scanf("%c",&c);
+        if(c == '-'){
+            send(sockfd,"-\0",strlen("-\0") + 1,0);
+            break;
         }
+        buffer[i] = c;
+        i++;
+        while(1){
+            char c;
+            scanf("%c",&c);
+            if(c == '\n'){
+                buffer[i] = '\0';
+                send(sockfd,buffer,strlen(buffer) + 1,0);
+                break;
+            }
+            buffer[i] = c;
+            i++;
+            if(i == PACKET_SIZE){
+                send(sockfd,buffer,PACKET_SIZE,0);
+                i = 0;
+            }       
+        }
+    
+        recv(sockfd,recv_buffer,PACKET_SIZE*2,0);
+        printf("%s",recv_buffer);      
     }
     close(sockfd);
+    printf("Connection with server terminated\n");
 
 
     return 0;
--- assgn1/server_it.c
+++ assgn1/server_it.c
@@ -8,9 +8,29 @@
 #include<unistd.h>
 #include <stdbool.h>
 #define PORT_NUM 1301
-
+#define PACKET_SIZE 20
+#define NO_OP '_'
+bool isOperator(char c){
+    if(c == '-' || c == '+' || c == '*' || c == '/')
+        return true;
+    else return false;
+}
+void calc(float present_num,char operator,float* result){
+    float prev_result = *result;
+    if(operator == '+')
+        *result = *result + present_num;
+    else if(operator == '*')
+        *result = *result * present_num;
+    else if(operator == '/')
+        *result = *result / present_num;
+    else if(operator == '-')
+        *result = *result - present_num;
+    else if(operator == '=')
+        *result = present_num;
+    printf("prev_result : %f presentnum : %f operator : %c result = %f\n",prev_result,present_num,operator,*result);
+}
 int main(){
-    char buffer[200];
+    char buffer[PACKET_SIZE];
     int sockfd;
     struct sockaddr_in serv_addr,cli_addr;
     
@@ -41,32 +61,86 @@ int main(){
     else{
         printf("Connection with client established\n");
     }
-    // while(1){
-    //     newsockfd = accept(sockfd,(struct sockaddr*)&cli_addr,&cli_len);
-    //     if(newsockfd < 0){
-    //         perror("Connection(accept) to client failed\n");
-    //         exit(0);
-    //     }
-    //     else{
-    //         printf("Connection with client established\n");
-    //     }
-    //     break;
-    // }
-    
-    bool continue_connection = true;
-    while(continue_connection){
-        recv(newsockfd,buffer,200,0);
-        int val = atoi(buffer);
-        if(val == -1){
-            printf("Connection Terminated\n");
-            close(newsockfd);
-            continue_connection = false;
-        }
-        else{
-            printf("Recieved the number : %d\n",val);
-        }
-    }
-    close(sockfd);
+        
+    bool done = false;
+    while(1){        
+        bool first = true;
+        bool decimal = false;
+        float num1 = 0,num2 = 0;
+        float present_num = 0;
+        float pow10 = 0.1;
+        char operator = '=';
+        bool end = false;
+        while(1){
+            recv(newsockfd,buffer,20,0);
+            printf("Before reading next buffer, present num is: %f",present_num);
+            if(first){
+                if(buffer[0] == '-'){
+                    done = true;
+                    break;
+                }
+                first = false;
 
+            }
+            for(int i = 0;i < 20;i++){
+                printf("character recived : %c\n",buffer[i]);
+                if(buffer[i] - '0' >= 0 && buffer[i] - '0' <= 9){
+                    
+                    if(decimal){
+                        present_num += (float)(buffer[i] - '0')*pow10;
+                        pow10 /= 10;
+                    }
+                    else present_num = present_num*10.0 + (float)(buffer[i] - '0');
+                    printf("present_num now : %f\n",present_num);
+                    
+                }
+                else if(buffer[i] == '.'){
+                    decimal = true;
+                }
+                else if(buffer[i] == ' '){
+                    // if(operator != NO_OP){
+                    //     calc(present_num,operator,&num1);
+                    //     operator = NO_OP;
+                    // }
+                    // present_num = 0;
+                    // decimal = false;
+                    // pow10 = 0.1;
+                    continue;
+                }
+                else if(isOperator(buffer[i])){
+                    if(operator != NO_OP){
+                        calc(present_num,operator,&num1);
+                        operator = NO_OP;
+                    }
+                    present_num = 0;
+                    decimal = false;
+                    pow10 = 0.1;
+                    operator = buffer[i];
+                }
+                else if(buffer[i] == '\0'){
+                    if(operator != NO_OP){
+                        calc(present_num,operator,&num1);
+                        operator = NO_OP;
+                    }
+                    end = true;
+                    break;
+                }
+            }
+            if(end){
+                char result[40];
+                sprintf(result,"Answer : %f\n",num1);
+                printf("%s",result);
+                send(newsockfd,result,strlen(result)+1,0);
+                break;
+            }
+            
+        }
+        if(done)
+                break;
+
+    }
+    close(newsockfd);
+    close(sockfd);
+    printf("Connection with client terminated\n");
     return 0;
 }
